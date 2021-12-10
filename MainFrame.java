ab51ef6608dd4952c97a9c8664022f2fa87ed318
Alex <kosymbaevalex@mail.ru>
2021-12-11 01:42:22 +0300

    Add podmunu in frame

--- MainFrame.java
+++ MainFrame.java
@@ -33,6 +33,43 @@ public class MainFrame extends JFrame{
 
     private GornerTableModel data;
 
+    protected void saveToTextFile(File selectedFile) {
+        try {
+            PrintStream out = new PrintStream(selectedFile);
+            out.println("Результаты табулирования многочлена по схеме Горнера");
+            out.print("Многочлен: ");
+            for (int i = 0; i < coefficients.length; i++) {
+                out.print(coefficients[i] + "*X^" +
+                        (coefficients.length - i - 1));
+                if (i != coefficients.length - 1)
+                    out.print(" + ");
+            }
+            out.println("");
+            out.println("Интервал от " + data.getFrom() + " до " +
+                    data.getTo() + " с шагом " + data.getStep());
+            out.println("====================================================");
+            for (int i = 0; i < data.getRowCount(); i++) {
+                out.println("Значение в точке " + data.getValueAt(i, 0)
+                        + " равно " + data.getValueAt(i, 1));
+            }
+            out.close();
+        } catch (FileNotFoundException e) {
+        }
+    }
+
+        protected void saveToGraphicsFile(File selectedFile) {
+        try {
+            DataOutputStream out = new DataOutputStream(new
+                    FileOutputStream(selectedFile));
+            for (int i = 0; i<data.getRowCount(); i++) {
+                out.writeDouble((Double)data.getValueAt(i,0));
+                out.writeDouble((Double)data.getValueAt(i,1));
+            }
+            out.close();
+        } catch (Exception e) {
+        }
+    }
+
 
     public MainFrame(Double[] coefficients) {
         super("Табулирование многочлена на отрезке по схеме Горнера");
@@ -49,7 +86,53 @@ public class MainFrame extends JFrame{
         menuBar.add(tableMenu);
         JMenu referenceMenu = new JMenu("Справка");
         menuBar.add(referenceMenu);
+        Action saveToTextAction = new AbstractAction("Сохранить в текстовый файл") {
+            public void actionPerformed(ActionEvent ev){
+                if (fileChooser==null) {
+                    fileChooser = new JFileChooser();
+                    fileChooser.setCurrentDirectory(new File("."));
+                }
+                if (fileChooser.showSaveDialog(MainFrame.this) == JFileChooser.APPROVE_OPTION) {
+                    saveToTextFile(fileChooser.getSelectedFile());
+                }
+            }
 
+        };
+        saveToTextMenuItem = fileMenu.add(saveToTextAction);
+        saveToTextMenuItem.setEnabled(false);
+        Action saveToGraphicsAction = new AbstractAction("Сохранить данные для построения графика") {
+            public void actionPerformed(ActionEvent event) {
+                if (fileChooser==null) {
+                    fileChooser = new JFileChooser();
+                    fileChooser.setCurrentDirectory(new File("."));
+                }
+                if (fileChooser.showSaveDialog(MainFrame.this) ==
+                        JFileChooser.APPROVE_OPTION);
+
+                saveToGraphicsFile(fileChooser.getSelectedFile());
+            }
+        };
+        saveToGraphicsMenuItem = fileMenu.add(saveToGraphicsAction);
+        saveToGraphicsMenuItem.setEnabled(false);
+        Action searchValueAction = new AbstractAction("Найти значение многочлена") {
+            public void actionPerformed(ActionEvent event) {
+// Запросить пользователя ввести искомую строку
+                String value =JOptionPane.showInputDialog(MainFrame.this, "Введите значение для поиска",
+                        "Поиск значения", JOptionPane.QUESTION_MESSAGE);
+                renderer.setNeedle(value);
+                getContentPane().repaint();
+            }
+        };
+        searchValueMenuItem = tableMenu.add(searchValueAction);
+        searchValueMenuItem.setEnabled(false);
+
+        Action aboutProgramAction = new AbstractAction("О программе") {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(MainFrame.this, "Косымбаев 10 группа");
+            }
+        };
+        aboutProgram = referenceMenu.add(aboutProgramAction);
+        aboutProgram.setEnabled(true);
 
     }
 
